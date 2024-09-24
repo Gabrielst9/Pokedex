0eee0a71f729472abf06827ea43bcdd46c63e2b5
Gabriel Tavares <gabrielgstell@gmail.com>
2024-09-24 20:54:30 -0300

    Implementado a pokedex básica inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,10 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+  ]
 };
